09da01f31759a60fb18de0a5bbaf87ccee0c1b5d
Daniel Butterfield <danielcbutter@gmail.com>
2024-03-23 14:32:07 -0400

    Add option to switch nodes/edges, Cerberus Dataset Loading Speed Increased, GNN functional

--- datasets.py
+++ datasets.py
@@ -1,56 +1,62 @@
 import torch
 import torch_geometric
-from torch_geometric.data import Data
+from torch_geometric.data import Data, Dataset
 from urdfParser import RobotURDF
 from csvParser import RobotCSV
 import networkx
 import numpy as np
 import matplotlib.pyplot as plt
 from rosbags.highlevel import AnyReader
-from torch_geometric.data import Dataset
 from pathlib import Path
 from itertools import islice
+import os
+from rosbags.rosbag2 import Writer
+from rosbags.typesys import Stores, get_typestore, get_types_from_msg
+from rosbags.interfaces import ConnectionExtRosbag2
+from typing import cast
 
 
 class CerberusStreetDataset(Dataset):
 
     def __init__(self,
-                 root=None,
+                 root,
+                 robotURDF: RobotURDF,
                  entries_to_use=None,
                  transform=None,
                  pre_transform=None,
                  pre_filter=None):
+
+        # Setup the directories for raw and processed data
+        self.root = root
         super().__init__(root, transform, pre_transform, pre_filter)
 
         # Setup the reader for reading rosbags
-        self.reader = AnyReader([Path(root)])
-        self.reader.open()
-        self.imu_gen = self.reader.messages(connections=[
-            x for x in self.reader.connections if x.topic == '/hardware_a1/imu'
-        ])
-        self.joint_gen = self.reader.messages(connections=[
-            x for x in self.reader.connections
-            if x.topic == '/hardware_a1/joint_foot'
+        path_to_bag = os.path.join(root, 'raw', 'street.bag')
+        reader = AnyReader([Path(path_to_bag)])
+        reader.open()
+        imu_gen = reader.messages(connections=[
+            x for x in reader.connections if x.topic == '/hardware_a1/imu'
         ])
 
         # Get the first index of the dataset.
-        for connection, timestamp, rawdata in self.imu_gen:
-            msg = self.reader.deserialize(rawdata, connection.msgtype)
+        for connection, timestamp, rawdata in imu_gen:
+            msg = reader.deserialize(rawdata, connection.msgtype)
             self.first_index = msg.header.seq
             break
 
         # Calculate the length of the dataset
-        self.length = sum(1 for _ in self.imu_gen)
+        self.length = sum(1 for _ in imu_gen)
         if entries_to_use is not None and entries_to_use <= self.length:
             self.length = entries_to_use
+        reader.close()
 
         # Open up the A1 urdf file and get the edge matrix
-        self.A1_URDF = RobotURDF('urdf_files/A1/a1.urdf',
-                                 'package://a1_description/',
-                                 'unitree_ros/robots/a1_description')
+        self.A1_URDF = robotURDF
         self.edge_matrix = self.A1_URDF.get_edge_index_matrix()
         self.edge_matrix_tensor = torch.tensor(self.edge_matrix,
                                                dtype=torch.long)
+        # Get node name to index mapping
+        self.urdf_name_to_index = self.A1_URDF.get_node_name_to_index_dict()
 
         # Map ROS names to array positions
         self.ros_name_in_index = [
@@ -80,102 +86,118 @@ class CerberusStreetDataset(Dataset):
             'RR_foot_fixed': 'RR_foot'
         }
 
-        # Define the ros names that contain our ground truth labels
-        self.ground_truth_ros_names = [
-            'FL_foot', 'FR_foot', 'RL_foot', 'RR_foot'
+        # Define the names that contain ground truth labels
+        self.ground_truth_urdf_names = [
+            'FL_foot_fixed', 'FR_foot_fixed', 'RL_foot_fixed', 'RR_foot_fixed'
         ]
+        self.ground_truth_ros_names = []
+        for urdf_name in self.ground_truth_urdf_names:
+            self.ground_truth_ros_names.append(self.urdf_to_ros_map[urdf_name])
 
-        # Define the joints that should recieve edge attributes
-        self.joints_for_attributes = [
+        # Define the nodes that should recieve features
+        self.nodes_for_attributes = [
             'FL_hip_joint', 'FL_thigh_joint', 'FL_calf_joint', 'FR_hip_joint',
             'FR_thigh_joint', 'FR_calf_joint', 'RL_hip_joint',
             'RL_thigh_joint', 'RL_calf_joint', 'RR_hip_joint',
             'RR_thigh_joint', 'RR_calf_joint'
         ]
 
-        # Close the reader
-        self.reader.close()
+    @property
+    def raw_file_names(self):
+        return ['street.bag']
+
+    def download(self):
+        raise Exception(
+            "Please download the bag file from https://drive.google.com/drive/folders/1PdCLMVRqS97Tc9VbJY12EUl9UHpVoO_X and put it in <dataset_dir>/raw/"
+        )
+
+    @property
+    def processed_file_names(self):
+        return ['processed.txt']
+
+    def process(self):
+        topic = '/hardware_a1/joint_foot'
+
+        # Set up a reader to read the rosbag
+        path_to_bag = os.path.join(self.root, 'raw', 'street.bag')
+        self.reader = AnyReader([Path(path_to_bag)])
+        self.reader.open()
+        self.joint_gen = self.reader.messages(connections=[
+            x for x in self.reader.connections if x.topic == topic
+        ])
+
+        # Iterate through every joint message and save important data in txt file
+        for connection, _timestamp, rawdata in self.joint_gen:
+            msg = self.reader.deserialize(rawdata, connection.msgtype)
+
+            with open(
+                    os.path.join(self.processed_dir,
+                                 str(msg.header.seq) + ".txt"), "w") as f:
+                arrays = [msg.position, msg.velocity, msg.effort]
+                for array in arrays:
+                    for val in array:
+                        f.write(str(val) + " ")
+                    f.write('\n')
+
+        # Write a txt file so we know we've finisehd
+        with open(os.path.join(self.processed_dir, "processed.txt"), "w") as f:
+            f.write(
+                "Bag files have been successfully written to this directory.")
 
     def len(self):
         return self.length
-
-    def get_val_from_generator(self, gen, idx, default=None):
-        # Quickly index into the generator and extract the
-        # value at the idx position
-        return next(islice(gen, idx, None), default)
 
     def get_position_of_ros_name(self, name):
         for i in range(0, len(self.ros_name_in_index)):
             if name == self.ros_name_in_index[i]:
                 return i
 
-    def find_matrix_indexes_from_connections(self, edge_connections):
-        edge_index = list()
-        for i in range(0, len(self.edge_matrix[0])):
-            vector = self.edge_matrix[:, i]
-            if (np.array_equal(vector, edge_connections)
-                    or np.array_equal(vector, np.flip(edge_connections))):
-                edge_index.append(i)
-        return edge_index
-
-    def create_edge_attr_matrix(self, msg):
-        # Load the edge matrix
-        edge_matrix = self.A1_URDF.get_edge_index_matrix()
-        edge_name_to_connection_dict = self.A1_URDF.get_edge_name_to_connections_dict(
-        )
-
-        # Create the edge_attribute matrix
-        edge_attrs = np.zeros((len(edge_matrix[0]), 2))
-
-        # For each joint specified
-        for urdf_joint_name in self.joints_for_attributes:
-
-            # Find the edge_attrs indexes of the particular joint
-            joint_con = edge_name_to_connection_dict[urdf_joint_name]
-            matrix_indexes = self.find_matrix_indexes_from_connections(
-                joint_con)
-
-            # Get the msg array index
-            msg_ind = self.get_position_of_ros_name(
-                self.urdf_to_ros_map[urdf_joint_name])
-
-            # Add the features to the edge_attrs matrix
-            for edge_index in matrix_indexes:
-                edge_attrs[edge_index, 0] = msg.position[msg_ind]
-                edge_attrs[edge_index, 1] = msg.velocity[msg_ind]
-
-        return edge_attrs
-
     def get(self, idx):
-        # Find the joint_foot message with the right index
-        self.reader.open()
-        connection, timestamp, rawdata = self.get_val_from_generator(
-            self.joint_gen, idx)
-        msg = self.reader.deserialize(rawdata, connection.msgtype)
+        # Open the file with the proper index
+        positions = []
+        velocities = []
+        efforts = []
+        with open(
+                os.path.join(self.processed_dir,
+                             str(self.first_index + idx) + ".txt"), 'r') as f:
+            line = f.readline().split(" ")[:-1]
+            for i in range(0, len(line)):
+                positions.append(float(line[i]))
+            line = f.readline().split(" ")[:-1]
+            for i in range(0, len(line)):
+                velocities.append(float(line[i]))
+            line = f.readline().split(" ")[:-1]
+            for i in range(0, len(line)):
+                efforts.append(float(line[i]))
 
         # Get the ground truth force labels
         ground_truth_labels = []
         for name in self.ground_truth_ros_names:
             ground_truth_labels.append(
-                msg.effort[self.get_position_of_ros_name(name)])
+                efforts[self.get_position_of_ros_name(name)])
 
-        # Create a note feature matrix (filled with ones)
-        node_features = torch.ones((self.A1_URDF.get_num_links(), 2),
-                                   dtype=torch.float)
+        # Create a note feature matrix
+        x = torch.ones((self.A1_URDF.get_num_nodes(), 2), dtype=torch.float)
 
-        # Get the edge attributes
-        edge_attrs_matrix = self.create_edge_attr_matrix(msg)
+        # For each joint specified
+        for urdf_node_name in self.nodes_for_attributes:
+
+            # Find the index of this particular node
+            node_index = self.urdf_name_to_index[urdf_node_name]
+
+            # Get the msg array index
+            msg_ind = self.get_position_of_ros_name(
+                self.urdf_to_ros_map[urdf_node_name])
+
+            # Add the features to x matrix
+            x[node_index, 0] = positions[msg_ind]
+            x[node_index, 1] = velocities[msg_ind]
 
         # Create the graph
-        graph = Data(x=node_features,
+        graph = Data(x=x,
                      edge_index=self.edge_matrix_tensor,
-                     edge_attr=torch.tensor(edge_attrs_matrix,
-                                            dtype=torch.float),
                      y=torch.tensor(ground_truth_labels, dtype=torch.float),
-                     num_nodes=self.A1_URDF.get_num_links())
-
-        # Close the reader
-        self.reader.close()
+                     num_nodes=self.A1_URDF.get_num_nodes())
 
         return graph
 
@@ -239,7 +261,7 @@ class HyQDataset(Dataset):
                      edge_attr=torch.tensor(edge_attrs_matrices[i],
                                             dtype=torch.float),
                      y=torch.tensor(y_all_graphs[i], dtype=torch.float),
-                     num_nodes=HyQ_URDF.get_num_links()))
+                     num_nodes=HyQ_URDF.get_num_nodes()))
         print("HyQ data load complete")
 
     def _find_edge_indexes_from_connections(self, edge_matrix,
@@ -352,7 +374,7 @@ def hyq_test():
     networkx.draw(nx_graph, pos=spring_layout)
     networkx.draw_networkx_labels(nx_graph,
                                   pos=spring_layout,
-                                  labels=HyQ_URDF.get_link_num_to_name_dict(),
+                                  labels=HyQ_URDF.get_node_num_to_name_dict(),
                                   verticalalignment='top',
                                   font_size=8)
     networkx.draw_networkx_edge_labels(nx_graph,
@@ -363,10 +385,48 @@ def hyq_test():
     plt.show()
 
 
+def a1_test():
+    # Load the A1 URDF file
+    A1_URDF = RobotURDF('urdf_files/A1/a1.urdf', 'package://a1_description/',
+                        'unitree_ros/robots/a1_description', True)
+
+    # Create the dataset
+    dataset = CerberusStreetDataset(
+        '/home/dlittleman/state-estimation-gnn/datasets/cerberus_street',
+        A1_URDF)
+
+    # Extract the first graph
+    graph: Data = dataset[0]
+
+    # Write the features onto the names
+    node_labels = A1_URDF.get_node_index_to_name_dict()
+    for i in range(0, len(graph.x)):
+        label = node_labels[i]
+        label += ": " + str(graph.x[i].numpy())
+        node_labels[i] = label
+
+    # Convert to networkx graph
+    nx_graph = torch_geometric.utils.to_networkx(graph, to_undirected=True)
+
+    # Draw the graph
+    spring_layout = networkx.spring_layout(nx_graph)
+    networkx.draw(nx_graph, pos=spring_layout)
+    networkx.draw_networkx_labels(nx_graph,
+                                  pos=spring_layout,
+                                  labels=node_labels,
+                                  verticalalignment='top',
+                                  font_size=8)
+    # networkx.draw_networkx_edge_labels(
+    #     nx_graph,
+    #     pos=spring_layout,
+    #     edge_labels=A1_URDF.get_edge_connections_to_name_dict(),
+    #     rotate=False,
+    #     font_size=7)
+    plt.show()
+
+
 def main():
-    cerb = CerberusStreetDataset(
-        '/home/dlittleman/state-estimation-gnn/datasets/street.bag')
-    print(cerb.get(263306))
+    a1_test()
 
 
 if __name__ == "__main__":
--- urdfParser.py
+++ urdfParser.py
@@ -1,11 +1,35 @@
 from urchin import URDF
+import urchin
 import os
 import numpy as np
 
 
 class RobotURDF():
 
-    def __init__(self, urdf_path, ros_builtin_path, urdf_to_desc_path):
+    class Node():
+        """
+        Simple class that holds a node's name.
+        """
+
+        def __init__(self, name):
+            self.name = name
+
+    class Edge():
+        """
+        Simple class that holds an edge's name, and
+        both connections.
+        """
+
+        def __init__(self, name, parent, child):
+            self.name = name
+            self.parent = parent
+            self.child = child
+
+    def __init__(self,
+                 urdf_path,
+                 ros_builtin_path,
+                 urdf_to_desc_path,
+                 swap_nodes_and_edges=False):
         """
         Constructor for RobotURDF class.
 
@@ -19,6 +43,10 @@ class RobotURDF():
             urdf_to_desc_path (str): The relative path from the urdf file to
                 the urdf description directory. This directory typically contains
                 folders like "meshes" and "urdf".
+            swap_nodes_and_edges (bool): If False, links in the URDF will be represented
+                as nodes, and joints will be represented as edges. If True, links will
+                be represented as edges, and joints will be represented as nodes. This
+                will drop links that aren't connected to two joints. Default=False.
         """
 
         # Define paths
@@ -33,6 +61,49 @@ class RobotURDF():
 
         # Load the URDF with updated paths
         self.robot_urdf = URDF.load(self.new_urdf_path)
+
+        # Setup nodes and edges
+        if not swap_nodes_and_edges:
+            self.nodes = []
+            for link in self.robot_urdf.links:
+                self.nodes.append(self.Node(name=link.name))
+            self.edges = []
+            for joint in self.robot_urdf.joints:
+                self.edges.append(
+                    self.Edge(name=joint.name,
+                              parent=joint.parent,
+                              child=joint.child))
+        else:
+            # Create Links from the Joints, to set as the nodes
+            self.nodes = []
+            for joint in self.robot_urdf.joints:
+                new_node = self.Node(name=joint.name)
+                self.nodes.append(new_node)
+
+            # Create joints from the links, to set as the edges
+            self.edges = []
+            for link in self.robot_urdf.links:
+                # Check that two joints connect to this link
+                connections = []
+                for joint in self.robot_urdf.joints:
+                    if (joint.parent == link.name or joint.child == link.name):
+                        connections.append(joint.name)
+
+                # If two joints do connect, add it as an edge
+                if len(connections) == 2:
+                    new_edge = self.Edge(name=link.name,
+                                         parent=connections[0],
+                                         child=connections[1])
+                    self.edges.append(new_edge)
+                if len(connections) > 2:
+                    # Setup an edge for each pair of connections
+                    count = 0
+                    for i in range(0, len(connections)):
+                        for j in range(i+1, len(connections)):
+                            self.edges.append(
+                                self.Edge(name=link.name + str(count),
+                                          parent=connections[i],
+                                          child=connections[j]))
 
     def create_updated_urdf_file(self):
         """
@@ -62,57 +133,57 @@ class RobotURDF():
         with open(self.new_urdf_path, 'w') as f:
             f.writelines(file_data)
 
-    def get_link_name_to_index_dict(self):
+    def get_node_name_to_index_dict(self):
         """
-        Return a dictionary that maps the link name to its
+        Return a dictionary that maps the node name to its
         corresponding index.
 
         Returns:
-            (dict[str, int]): A dictinoary that maps link name
+            (dict[str, int]): A dictinoary that maps node name
                 to index.
         """
-        link_names = []
-        for link in self.robot_urdf.links:
-            link_names.append(link.name)
-        return dict(zip(link_names, range(len(self.robot_urdf.links))))
+        node_names = []
+        for node in self.nodes:
+            node_names.append(node.name)
+        return dict(zip(node_names, range(len(self.nodes))))
 
-    def get_link_index_to_name_dict(self):
+    def get_node_index_to_name_dict(self):
         """
-        Return a dictionary that maps the link index to its
+        Return a dictionary that maps the node index to its
         name
 
         Returns:
-            (dict[int, str]): A dictionary that maps link index
+            (dict[int, str]): A dictionary that maps node index
                 to name.
         """
 
-        link_names = []
-        for link in self.robot_urdf.links:
-            link_names.append(link.name)
-        link_dict = dict(zip(range(len(self.robot_urdf.links)), link_names))
-        return link_dict
+        node_names = []
+        for node in self.nodes:
+            node_names.append(node.name)
+        node_dict = dict(zip(range(len(self.nodes)), node_names))
+        return node_dict
 
     def get_edge_index_matrix(self):
         """
         Return the edge connectivity matrix, which defines each edge connection
-        to each joint. This matrix is the 'edge_index' matrix passed to the PyTorch
+        to each node. This matrix is the 'edge_index' matrix passed to the PyTorch
         Geometric Data class: https://pytorch-geometric.readthedocs.io/en/latest/generated/torch_geometric.data.Data.html#torch_geometric.data.Data . However, it will need to be converted to a torch.Tensor first.
 
         Returns:
             edge_index (np.array): A 2xN matrix, where N is the twice the number of
                 edge connections. This is because each connection must 
                 be put into the matrix twice, as each edge is bidirectional
-                For example, a connection between links 0 and 1 will be
+                For example, a connection between nodes 0 and 1 will be
                 found in the edge_index matrix as [[0, 1], [1, 0]].
 
         """
-        link_dict = self.get_link_name_to_index_dict()
+        node_dict = self.get_node_name_to_index_dict()
 
-        # Iterate through joints, and add to the edge matrix
+        # Iterate through edges, and add to the edge matrix
         edge_matrix = None
-        for joint in self.robot_urdf.joints:
-            a_index = link_dict[joint.parent]
-            b_index = link_dict[joint.child]
+        for edge in self.edges:
+            a_index = node_dict[edge.parent]
+            b_index = node_dict[edge.child]
             edge_vector = np.array([[a_index, b_index], [b_index, a_index]])
             if edge_matrix is None:
                 edge_matrix = edge_vector
@@ -122,59 +193,59 @@ class RobotURDF():
 
         return edge_matrix
 
-    def get_num_links(self):
+    def get_num_nodes(self):
         """
-        Return the number of links in the URDF file.
+        Return the number of nodes in the URDF file.
 
         Returns:
-            (int): Number of link nodes in URDF.
+            (int): Number of node nodes in URDF.
         """
-        return len(self.robot_urdf.links)
+        return len(self.nodes)
 
     def get_edge_connections_to_name_dict(self):
         """
-        Return a dictionary that maps a tuple of two link indices
+        Return a dictionary that maps a tuple of two node indices
         and return the name of the edge that connects to both of those 
-        links.
+        nodes.
 
         Returns:
-            joint_dict (dict[tuple(int, int), str]): This dictionary
-                takes a tuple of two link indices. It 
+            edge_dict (dict[tuple(int, int), str]): This dictionary
+                takes a tuple of two node indices. It 
                 returns the name of the edge that connects them.
         """
 
-        link_dict = self.get_link_name_to_num_dict()
+        node_dict = self.get_node_name_to_index_dict()
 
-        # Create a dictionary to map edge pair to a joint name
-        joint_dict = {}
-        for joint in self.robot_urdf.joints:
-            joint_dict[(link_dict[joint.parent],
-                        link_dict[joint.child])] = joint.name
-            joint_dict[(link_dict[joint.child],
-                        link_dict[joint.parent])] = joint.name
+        # Create a dictionary to map edge pair to an edge name
+        edge_dict = {}
+        for edge in self.edges:
+            edge_dict[(node_dict[edge.parent],
+                       node_dict[edge.child])] = edge.name
+            edge_dict[(node_dict[edge.child],
+                       node_dict[edge.parent])] = edge.name
 
-        return joint_dict
+        return edge_dict
 
     def get_edge_name_to_connections_dict(self):
         """
-        Return a dictionary that maps the edge name to the links
+        Return a dictionary that maps the edge name to the nodes
         it's connected to.
 
         Returns:
-            joint_dict (dict[str, np.array([int, int])]): This dictionary
+            edge_dict (dict[str, np.array([int, int])]): This dictionary
                 takes the name of an edge in the URDF as input. It 
                 returns an np.array that contains the indices of the 
-                two links that this edge is connected to.
+                two nodes that this edge is connected to.
         """
 
-        link_dict = self.get_link_name_to_index_dict()
+        node_dict = self.get_node_name_to_index_dict()
 
-        # Create a dictionary to map edge pair to a joint name
-        joint_dict = {}
-        for joint in self.robot_urdf.joints:
-            joint_dict[joint.name] = np.array(
-                [link_dict[joint.parent], link_dict[joint.child]])
-        return joint_dict
+        # Create a dictionary to map edge name pair to its node connections
+        edge_dict = {}
+        for edge in self.edges:
+            edge_dict[edge.name] = np.array(
+                [node_dict[edge.parent], node_dict[edge.child]])
+        return edge_dict
 
     def display_URDF_info(self):
         """
@@ -214,7 +285,7 @@ def main():
     print("Edge Matrix (HyQ): ", HyQ_URDF.get_edge_index_matrix())
 
     A1_URDF = RobotURDF('urdf_files/A1/a1.urdf', 'package://a1_description/',
-                        'unitree_ros/robots/a1_description')
+                        'unitree_ros/robots/a1_description', True)
     A1_URDF.display_URDF_info()
     print("Edge Matrix (A1): ", A1_URDF.get_edge_index_matrix())
 
